5dc69c621a7e121a04039ce72f33d091e36311e3
Jason Zhou <jzhou35@buffalo.edu>
2021-03-17 19:42:29 -0400

    Fixed sizing of cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,11 @@ import {Card, Icon} from 'semantic-ui-react'
 
 let projects = [{front:"https://github.com/Jasonnnz/jobra_frontend", back:"https://github.com/Jasonnnz/jobra_backend", image:jobra, name: "Jobra", description: "A React application inspired by “Trello”, that allows users to keep track of job applications with a data layer to allow the creation of ad hoc charts.", languages: "React, Ruby on Rails, PostgreSQL, ChartJS"},
                 {front:"https://github.com/Jasonnnz/yourseum_frontend", back:"https://github.com/Jasonnnz/yourseum_backend", image:yourseum, name: "Yourseum", description: "A React application that takes favorite masterpieces out of the museum and places selections on a user’s screen.", languages: "React, Ruby on Rails, PostgreSQL, Netlify, Heroku"},
-                {front:"https://github.com/Jasonnnz/steam0.5_frontend", back:"https://github.com/Jasonnnz/steam0.5_backend", image:steam, name: "Steam0.5", description: "A single-page application inspired by the existing games application, 'Steam', that allows a user to connect and find common game interests with other users.", languages: "JavaScript, Ruby on Rails, PostgreSQL, HTML, CSS"},]
+                {front:"https://github.com/Jasonnnz/steam0.5_frontend", back:"https://github.com/Jasonnnz/steam0.5_backend", image:steam, name: "Steam0.5", description: "A single-page application that allows a user to connect and find common game interests with other users.", languages: "JavaScript, Ruby on Rails, PostgreSQL, HTML, CSS"},]
 // {front:"https://github.com/Jasonnnz/study_buddy", image:studybuddy, name: "StudyBuddy", description: "A JavaScript application that allows users to connect with other students and see what classes they're taking.", languages: "JavaScript, Ruby on Rails, SQLite3"}
 function Projects(){
     let projs = projects.map((p) => {
-        return <Card style={{height:"55%", width:"23%"}} 
+        return <Card style={{height:"60%", width:"23%"}} 
         image={p.image} 
         header={p.name} 
         description={p.description} 
@@ -27,7 +27,7 @@ function Projects(){
             <Card.Group itemsPerRow={4} className="project-list">
                 {projs}
                 <Card 
-                style={{height:"55%", width:"23%"}}
+                style={{height:"60%", width:"23%"}}
                 image={studybuddy} 
                 header="StudyBuddy" 
                 description="A JavaScript application that allows users to connect with other students and see what classes they're taking." 
