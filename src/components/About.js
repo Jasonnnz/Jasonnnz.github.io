02f26c9750847d607a15560d77ca9af84524720a
Jason Zhou <jzhou35@buffalo.edu>
2021-03-17 13:13:02 -0400

    language icons added. finished aboout page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ function About(){
             <div className="inner-about">
                 <div className="summary">
                     <h3 style={{color:"black"}}>About</h3>
-                    <h1 style={{color:"black"}}>Let me introduce myself.</h1>
                     <div className="picture-description">
                         <img src={picture3} alt="Profile Picture" height="150px" width="150px"></img>
                         <div className="description-div"><p className="description">
@@ -15,8 +14,42 @@ function About(){
                     </div>
                 </div>
                 <div className="about-bottom-div">
-                    <div className="my-profile"></div>
-                    <div className="skills"></div>
+                    <div className="proficient-skills">
+                        <div className="skills-list-div">
+                            <h2 className="skills-title">Proficient</h2>
+                            <ul className="skills-list">
+                                <li><i className="devicon-javascript-plain colored"></i></li>
+                                <li><i className="devicon-react-original-wordmark colored"></i></li>
+                                <li><i className="devicon-rails-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-html5-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-css3-plain-wordmark colored"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="familiar-skills">
+                        <div className="skills-list-div">
+                            <h2 className="skills-title">Familiar</h2>
+                            <ul className="skills-list">
+                                <li><i className="devicon-python-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-java-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-postgresql-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-heroku-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-git-plain-wordmark colored"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="want-to-learn-skills">
+                        <div className="skills-list-div">
+                            <h2 className="skills-title">Interested in learning</h2>
+                            <ul className="skills-list">
+                                <li><i className="devicon-swift-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-angularjs-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-django-plain colored"></i></li>
+                                <li><i className="devicon-docker-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-kubernetes-plain-wordmark colored"></i></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
