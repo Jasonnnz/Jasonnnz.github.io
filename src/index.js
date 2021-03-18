fdb6a241131543624c2f2b4e2645efdae63f2801
Jason Zhou <jzhou35@buffalo.edu>
2021-03-18 15:49:59 -0400

    Using HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/">
     <App />
   </Router>,
   document.getElementById('root')
