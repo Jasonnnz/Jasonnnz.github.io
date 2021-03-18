9325f1964124caf0babd92d37c195f291bfacdb3
Jason Zhou <jzhou35@buffalo.edu>
2021-03-18 11:07:34 -0400

    Edited description of jobra

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,9 @@ import studybuddy from '../images/studybuddy.png'
 import { FaCaretDown } from "react-icons/fa"
 import {Card, Icon} from 'semantic-ui-react'
 
-let projects = [{front:"https://github.com/Jasonnnz/jobra_frontend", back:"https://github.com/Jasonnnz/jobra_backend", image:jobra, name: "Jobra", description: "A React application inspired by “Trello”, that allows users to keep track of job applications with a data layer to allow the creation of ad hoc charts.", languages: "React, Ruby on Rails, PostgreSQL, ChartJS"},
+let projects = [{front:"https://github.com/Jasonnnz/jobra_frontend", back:"https://github.com/Jasonnnz/jobra_backend", image:jobra, name: "Jobra", description: "A React application that allows users to keep track of job applications with a data layer to allow the creation of ad hoc charts.", languages: "React, Ruby on Rails, PostgreSQL, ChartJS"},
                 {front:"https://github.com/Jasonnnz/yourseum_frontend", back:"https://github.com/Jasonnnz/yourseum_backend", image:yourseum, name: "Yourseum", description: "A React application that takes favorite masterpieces out of the museum and places selections on a user’s screen.", languages: "React, Ruby on Rails, PostgreSQL, Netlify, Heroku"},
-                {front:"https://github.com/Jasonnnz/steam0.5_frontend", back:"https://github.com/Jasonnnz/steam0.5_backend", image:steam, name: "Steam0.5", description: "A single-page application that allows a user to connect and find common game interests with other users.", languages: "JavaScript, Ruby on Rails, PostgreSQL, HTML, CSS"},]
-// {front:"https://github.com/Jasonnnz/study_buddy", image:studybuddy, name: "StudyBuddy", description: "A JavaScript application that allows users to connect with other students and see what classes they're taking.", languages: "JavaScript, Ruby on Rails, SQLite3"}
+                {front:"https://github.com/Jasonnnz/steam0.5_frontend", back:"https://github.com/Jasonnnz/steam0.5_backend", image:steam, name: "Steam0.5", description: "A single-page application that allows a user to connect and find common game interests with other users.", languages: "JavaScript, Ruby on Rails, HTML, CSS"}]
 function Projects(){
     let projs = projects.map((p) => {
         return <Card style={{height:"68%", width:"23%", boxShadow:"5px 5px 7px 5px black"}} 
